e3e8b6a0eeadf5567ce9e357811773e14cfe1288
ayoubmehd <safi-_-gio-raja@live.fr>
2021-04-25 11:53:05 +0000

    feat: change how MVC work use url instead of $_GET paramatre

--- index.php
+++ index.php
@@ -2,30 +2,38 @@
 
 $action = "";
 $ctr = "";
+$params  = explode("/", ltrim($_SERVER["REQUEST_URI"], "/"));
+// var_dump(explode("/", ltrim($_SERVER["REQUEST_URI"], "/")));
 
 require_once "config/globals.php";
 
-if (!isset($_GET["ctr"])) {
+if (!isset($params[0]) || $params[0] === "") {
     $ctr = "home";
 } else {
-    $ctr = $_GET["ctr"];
+    $ctr = $params[0];
 }
 
-if (!isset($_GET["action"])) {
+if (!isset($params[1]) || $params[1] === "") {
     $action = "index";
 } else {
-    $ctr = $_GET["ctr"];
+    $action = $params[1];
 }
+// var_dump([
+//     "controller" => $ctr,
+//     "action" => $action
+// ]);
 
 $ctr = ucfirst($ctr);
+
 
 $file = "./controllers/" . $ctr . ".controller.php";
 if (file_exists($file)) {
     require_once $file;
-    if (class_exists($ctr)) {
-        $app = new $ctr . "Controller"();
+    $ctrClass = $ctr . "Controller";
+    if (class_exists($ctrClass)) {
+        $app = new $ctrClass();
     }
     if (method_exists($app, $action)) {
-        $app->$action();
+        $app->$action(array_slice($params, 2, sizeof($params)));
     }
 }
