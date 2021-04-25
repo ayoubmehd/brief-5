2c4f8bf6bf2d4ed6d7307715541f4c261a9d5044
ayoubmehd <safi-_-gio-raja@live.fr>
2021-04-25 12:24:44 +0000

    chore(autoload): add autoload gloabaly

--- config/globals.php
+++ config/globals.php
@@ -3,5 +3,6 @@
 
 require_once "constant.php";
 require_once "functions.php";
+require_once "autoload.php";
 require_once __DIR__ . "/../controllers/Abstruct.controller.php";
 require_once __DIR__ . "/../models/Abstruct.php";
