803812e60c98181890e37ebad7bca45af23c994c
ayoubmehd <safi-_-gio-raja@live.fr>
2021-04-25 11:55:55 +0000

    chore: require constant and functions file globally

--- config/globals.php
+++ config/globals.php
@@ -1,4 +1,7 @@
 <?php
 
+
+require_once "constant.php";
+require_once "functions.php";
 require_once __DIR__ . "/../controllers/Abstruct.controller.php";
 require_once __DIR__ . "/../models/Abstruct.php";
