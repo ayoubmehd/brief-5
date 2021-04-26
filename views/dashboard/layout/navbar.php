664bc0f5f214cf9aef90b47cc48b0dcc4a390339
ayoubmehd <safi-_-gio-raja@live.fr>
2021-04-26 12:47:00 +0000

    style: add dashboard layout

---
+++ views/dashboard/layout/navbar.php
@@ -0,0 +1,15 @@
+<div class="col ml-n3 d-flex flex-column">
+
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <ul class="nav navbar-nav ml-auto">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">User</a>
+                <div class="dropdown-menu" style="left: unset; right: 0">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Action</a>
+                </div>
+            </li>
+        </ul>
+    </nav>
---
+++ views/dashboard/layout/sidebar.php
@@ -0,0 +1,32 @@
+<div class="container-fluid pl-0">
+
+    <div class="row">
+        <div class="col-md-3 mr-n2">
+            <nav class="navbar navbar-expand-sm navbar-dark bg-dark flex-column">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation"></button>
+                <form class="w-100 my-2 my-lg-0 align-self-start">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                </form>
+                <div class="collapse navbar-collapse flex-column align-self-start w-100" id="collapsibleNavId">
+                    <ul class="nav nav-tabs flex-column w-100" style="height: 100vh;">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-white">Active</a>
+                        </li>
+                        <!-- <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-white" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                        <div class="dropdown-menu bg-dark">
+                            <a class="dropdown-item text-white" href="#">Action</a>
+                            <a class="dropdown-item text-white" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item text-white" href="#">Action</a>
+                        </div>
+                    </li> -->
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-white">Another link</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+        </div>
