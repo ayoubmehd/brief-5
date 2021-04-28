218fbe01db17cebfa95bfe5572afbdadf7ccc110
ayoubmehd <safi-_-gio-raja@live.fr>
2021-04-28 12:21:11 +0000

    chore(database): add lcoal databse credentials

--- config/constant.php
+++ config/constant.php
@@ -4,7 +4,7 @@ define('BASE_URL', 'http://brief-5.test');
 define('BASE_PATH', __DIR__ . "/../");
 
 // Database constants
-define('DB_NAME', "login_poo");
+define('DB_NAME', "brief-5-gestion-emploit");
 define('DB_HOST', "localhost");
 define('DB_USER', "root");
 define('DB_PASS', "");
