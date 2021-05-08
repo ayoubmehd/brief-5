3d226ebb2a4263d8c0b2b2b56202b8565d965b94
ayoubmehd <safi-_-gio-raja@live.fr>
2021-05-08 13:00:38 +0000

    fix: watch command not working

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,16 @@ const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
 //compile scss into css
-function style() {
-    return gulp.src('assets/css/**/*.scss')
+function style(cb) {
+    gulp.src('assets/css/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('assets/css'))
         .pipe(browserSync.stream());
+    cb();
 }
 
 function watch() {
-    gulp.watch('src/scss/**/*.scss', style)
+    gulp.watch('assets/css/*.scss', style)
 }
 
-exports.style = style;
 exports.watch = watch;
