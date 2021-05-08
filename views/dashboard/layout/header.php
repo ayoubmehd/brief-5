bc80f07ca1d42d34b7082b8d04fbaedf6cca6b80
ayoubmehd <safi-_-gio-raja@live.fr>
2021-05-08 13:02:38 +0000

    refactor: import compiled css file instead of cdn

--- views/dashboard/layout/header.php
+++ views/dashboard/layout/header.php
@@ -6,15 +6,10 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <style>
-        td input,
-        td select {
-            width: 100%;
-        }
-    </style>
+
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo BASE_URL ?>/assets/css/style.css">
 </head>
 
 <body>
