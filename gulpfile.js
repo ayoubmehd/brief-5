432b03e27cdfa503068f21e178ab7d960c760bca
ayoubmehd <safi-_-gio-raja@live.fr>
2021-05-29 15:10:40 +0100

    build: browser sync package

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,11 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
 
 //compile scss into css
 function style(cb) {
     gulp.src('assets/css/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('assets/css'))
-        .pipe(browserSync.stream());
+        .pipe(gulp.dest('assets/css'));
     cb();
 }
 
