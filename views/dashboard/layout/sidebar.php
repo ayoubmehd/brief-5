878574f9279f816594d79f95fd992028f5456c92
ayoubmehd <safi-_-gio-raja@live.fr>
2021-05-30 10:24:21 +0100

    (auth): Hide sidebar links for ensegniant (group, salle, Matiere)

--- views/dashboard/layout/sidebar.php
+++ views/dashboard/layout/sidebar.php
@@ -5,32 +5,23 @@
             <nav class="navbar navbar-expand-sm navbar-dark bg-dark flex-column">
                 <a class="navbar-brand" href="#">Navbar</a>
                 <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation"></button>
-                <form class="w-100 my-2 my-lg-0 align-self-start">
-                    <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                </form>
                 <div class="collapse navbar-collapse flex-column align-self-start w-100" id="collapsibleNavId">
                     <ul class="nav nav-tabs flex-column w-100" style="height: 100vh;">
-                        <li class="nav-item">
-                            <a href="/dashboard/group" class="nav-link text-white">Group</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/dashboard/matiere" class="nav-link text-white">Matiere</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/dashboard/salle" class="nav-link text-white">Salle</a>
-                        </li>
+                        <?php if ($this->isAdmin()) : ?>
+                            <li class="nav-item">
+                                <a href="/dashboard/group" class="nav-link text-white">Group</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/dashboard/matiere" class="nav-link text-white">Matiere</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/dashboard/salle" class="nav-link text-white">Salle</a>
+                            </li>
+                        <?php endif ?>
                         <li class="nav-item">
                             <a href="/dashboard/suiver" class="nav-link text-white">Reservations</a>
                         </li>
-                        <!-- <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle text-white" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                        <div class="dropdown-menu bg-dark">
-                            <a class="dropdown-item text-white" href="#">Action</a>
-                            <a class="dropdown-item text-white" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item text-white" href="#">Action</a>
-                        </div>
-                    </li> -->
+
                     </ul>
                 </div>
             </nav>
