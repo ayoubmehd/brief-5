fb7b6a8ab48b9d2bba90ae01ec49c46d0cd79e80
ayoubmehd <safi-_-gio-raja@live.fr>
2021-05-30 10:58:33 +0100

    fix: revers condition to check if not logged in make regular user access groupe all remove suiver functions that is not print json

--- controllers/Api.controller.php
+++ controllers/Api.controller.php
@@ -12,7 +12,7 @@ class ApiController extends AbstructController
     // Crud Group
     public function group_all()
     {
-        if ($this->isAdmin()) {
+        if (!$this->isAdmin()) {
             echo json_encode(["error" => "Only admin have access to this page"]);
             return;
         }
@@ -27,7 +27,7 @@ class ApiController extends AbstructController
     // Crud Matiere
     public function matiere_all()
     {
-        if ($this->isAdmin()) {
+        if (!$this->isAdmin()) {
             echo json_encode(["error" => "Only admin have access to this page"]);
             return;
         }
@@ -43,7 +43,7 @@ class ApiController extends AbstructController
     // Crud Salle
     public function salle_all()
     {
-        if ($this->isAdmin()) {
+        if (!$this->isAdmin()) {
             echo json_encode(["error" => "Only admin have access to this page"]);
             return;
         }
@@ -56,7 +56,7 @@ class ApiController extends AbstructController
 
     public function salle_aviable()
     {
-        if ($this->isLoggedIn()) {
+        if (!$this->isLoggedIn()) {
             echo json_encode(["error" => "You need to login to access to this ressource"]);
             return;
         }
@@ -88,7 +88,7 @@ class ApiController extends AbstructController
     // Crud Salle
     public function suiver_all()
     {
-        if ($this->isLoggedIn()) {
+        if (!$this->isLoggedIn()) {
             echo json_encode(["error" => "You need to login to access to this ressource"]);
             return;
         }
@@ -101,82 +101,6 @@ class ApiController extends AbstructController
         echo json_encode(["data" => $obj->result]);
     }
 
-    public function add_suiver()
-    {
-        if (!isset($_POST["add"])) return $this->redirect("/dashboard/suiver");
-        if (!isset($_POST["Ensegniant_id"]) && $this->isAdmin()) return $this->redirect("/dashboard/suiver");
-        if (!isset($_POST["Groupe_id"])) return $this->redirect("/dashboard/suiver");
-        if (!isset($_POST["Salle_id"])) return $this->redirect("/dashboard/suiver");
-        if (!isset($_POST["jour"])) return $this->redirect("/dashboard/suiver");
-        if (!isset($_POST["de"])) return $this->redirect("/dashboard/suiver");
-        if (!isset($_POST["a"])) return $this->redirect("/dashboard/suiver");
-
-        $user = new User();
-        $user->select(["Ensegniant_id"], ["id" => $this->userid()]);
-        $currentUserEnsegniantId = isset($user->result[0]) ? $user->result[0]["Ensegniant_id"] : null;
-
-        $Ensegniant_id = $this->isAdmin() ? $_POST["Ensegniant_id"] : $currentUserEnsegniantId;
-
-        $Groupe_id = $_POST["Groupe_id"];
-        $Salle_id = $_POST["Salle_id"];
-        $jour = $_POST["jour"];
-        $de = $_POST["de"];
-        $a = $_POST["a"];
-        $obj = new Suiver();
-
-        $obj->insert([
-            "Ensegniant_id" => $Ensegniant_id,
-            "Groupe_id" => $Groupe_id,
-            "Salle_id" => $Salle_id,
-            "jour" => $jour,
-            "de" => $de,
-            "a" => $a,
-        ]);
-
-        return $this->redirect("/dashboard/suiver");
-    }
-
-    public function delete_suiver($id)
-    {
-        $obj = new Salle();
-        $obj->delete("id = $id");
-
-        return $this->redirect("/dashboard/salle");
-    }
-
-    public function edit_suiver($id)
-    {
-        if (!isset($_POST["edit"])) return $this->redirect("/dashboard/suiver");
-        if (!isset($_POST["Ensegniant_id"]) && $this->isAdmin()) return $this->redirect("/dashboard/suiver");
-        if (!isset($_POST["Groupe_id"])) return $this->redirect("/dashboard/suiver");
-        if (!isset($_POST["Salle_id"])) return $this->redirect("/dashboard/suiver");
-        if (!isset($_POST["jour"])) return $this->redirect("/dashboard/suiver");
-        if (!isset($_POST["de"])) return $this->redirect("/dashboard/suiver");
-        if (!isset($_POST["a"])) return $this->redirect("/dashboard/suiver");
-
-        $user = new User();
-        $user->select(["Ensegniant_id"], ["id" => $this->userid()]);
-        $currentUserEnsegniantId = isset($user->result[0]) ? $user->result[0]["Ensegniant_id"] : null;
-
-        $Ensegniant_id = $this->isAdmin() ? $_POST["Ensegniant_id"] : $currentUserEnsegniantId;
-        $Groupe_id = $_POST["Groupe_id"];
-        $Salle_id = $_POST["Salle_id"];
-        $jour = $_POST["jour"];
-        $de = $_POST["de"];
-        $a = $_POST["a"];
-        $obj = new Suiver();
-
-        $obj->update([
-            "Ensegniant_id" => $Ensegniant_id,
-            "Groupe_id" => $Groupe_id,
-            "Salle_id" => $Salle_id,
-            "jour" => $jour,
-            "de" => $de,
-            "a" => $a
-        ], ["id" => $id]);
-
-        return $this->redirect("/dashboard/suiver");
-    }
 
     public function ensegniant_all()
     {
